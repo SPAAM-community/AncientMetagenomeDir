0eb0bc927febebe2e3fa14f7402b81f81047f4e4
James Fellows Yates <jfy133@gmail.com>
2020-12-11 21:20:04 +0100

    Add live update code and workflow
    Former-commit-id: a899eb729fb48ad017bf18fe6845fe60df64f6ad

---
+++ assets/analysis/plot_maps.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(maps)
+
+source("assets/analysis/functions.R")
+source("assets/analysis/design_assets.R")
+
+raw_hostmetagenome <- load_thedir_data("ancientmetagenome-hostassociated/ancientmetagenome-hostassociated.tsv", "Host Associated Metagenome") 
+raw_hostsinglegenome <- load_thedir_data("ancientsinglegenome-hostassociated/ancientsinglegenome-hostassociated.tsv", "Host Associated Single Genome") 
+raw_environmental <- load_thedir_data("ancientmetagenome-environmental/ancientmetagenome-environmental.tsv", "Environmental Metagenome") 
+#raw_anthropogenic <- load_thedir_data("../../ancientmetagenome-anthropogenic/ancientmetagenome-anthropogenic.tsv", "Anthropogenic Metagenome") 
+
+data_map <- stats_map(raw_hostmetagenome, raw_hostsinglegenome, raw_environmental)
+
+figure_map <- plot_map(data_map)
+
+ggsave("AncientMetagenomeDir-Sample_Map.pdf",
+       path = "assets/analysis/live/",
+       figure_map,
+       device = cairo_pdf(),
+       units = "in",
+       width = 3.5,
+       height = 7
+)
+
+ggsave("AncientMetagenomeDir-Sample_Map.png",
+       path = "assets/analysis/live/",
+       figure_map,
+       device = "png",
+       units = "in",
+       width = 3.5,
+       height = 7
+)

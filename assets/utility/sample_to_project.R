1242e10fe445002f97c47acc25544d4efc15f6af
James Fellows Yates <jfy133@gmail.com>
2020-12-22 08:03:43 +0100

    Start to make applicable to lots at once

--- assets/utility/sample_to_project.R
+++ assets/utility/sample_to_project.R
@@ -3,16 +3,22 @@ library(jsonlite)
 library(httr)
 library(xml2)
 
-path2 <- "https://www.ebi.ac.uk/ena/browser/api/xml/SRS4625244?download=false&gzip=false"
+find_project_accession <- function(accession){
+  sample_res <- paste0("https://www.ebi.ac.uk/ena/browser/api/xml/", accession, "?download=false&gzip=false") %>% GET(url = .) %>% content %>% as_list
+  secondary_proj_id <- sample_res$SAMPLE_SET$SAMPLE$SAMPLE_LINKS$SAMPLE_LINK$XREF_LINK$ID[[1]]
+  secondary_res <- paste0("https://www.ebi.ac.uk/ena/browser/api/xml/", secondary_proj_id, "?download=false&gzip=false") %>% GET(url = .) %>% content %>% as_list
+  return(secondary_res$STUDY_SET$STUDY$IDENTIFIERS$SECONDARY_ID[[1]])
+}
 
-r <- GET(url = path2)
+find_multiple_project_accession <- function(accession_list) {
+  acc_vec <- str_split(accession_list, ",") %>% unlist
+  proj_res <- map(acc_vec, find_project_accession) %>% unlist %>% unique
+  return(paste0(proj_res, collapse = ","))
+}
 
-res <- str(content(r))
+#find_project_accession("SRS4625244")
 
-output <- as_list(content(r))
+ancient_metagenome <- read_tsv("ancientmetagenome-hostassociated/ancientmetagenome-hostassociated.tsv")
 
-secondary_proj <- output$SAMPLE_SET$SAMPLE$SAMPLE_LINKS$SAMPLE_LINK$XREF_LINK$ID[[1]]
+ancient_metagenome <- mutate(ancient_metagenome, archive_project_accession = map(archive_accession, find_multiple_project_accession))
 
-secondary_proj_rest <- paste0("https://www.ebi.ac.uk/ena/browser/api/xml/", secondary_proj, "?download=false&gzip=false") %>% GET(url = .) %>% content %>% as_list
-
-secondary_proj_rest$STUDY_SET$STUDY$IDENTIFIERS$SECONDARY_ID[[1]]

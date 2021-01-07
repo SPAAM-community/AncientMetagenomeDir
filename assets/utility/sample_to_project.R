87fcb885e189b4845f811e861c39c8531321f040
James Fellows Yates <jfy133@gmail.com>
2021-01-07 09:39:25 +0100

    Started work on multi-accession in one go
    Former-commit-id: 1e0328665346de89ae3917b9698a7e0370cf7914

--- assets/utility/sample_to_project.R
+++ assets/utility/sample_to_project.R
@@ -22,3 +22,15 @@ ancient_metagenome <- read_tsv("ancientmetagenome-hostassociated/ancientmetageno
 
 ancient_metagenome <- mutate(ancient_metagenome, archive_project_accession = map(archive_accession, find_multiple_project_accession))
 
+## Multi test
+accession <- paste0(ancient_metagenome$archive_accession[100:104], collapse ="%2C")
+search_url <- paste0("https://www.ebi.ac.uk/ena/browser/api/xml/", accession)
+GET(url = search_url, timeout(60)) %>% content
+
+## BIG TEST
+multi_accession <- ancient_metagenome %>% filter(archive %in% c("ENA","SRA")) %>% pull(archive_accession) %>% str_split(",") %>% unlist
+accession <- paste0(multi_accession, collapse ="%2C")
+search_url <- paste0("https://www.ebi.ac.uk/ena/browser/api/xml/", accession)
+search_res <- GET(url = search_url)
+
+## There is a bad ID somewhere!!! find it! Might need to do a loop...

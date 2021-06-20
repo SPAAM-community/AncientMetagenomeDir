9c88a618e0cdac93c9e74222f81f37e9a487d2cf
James Fellows Yates <jfy133@gmail.com>
2021-06-20 17:26:40 +0200

    Start script conversion

---
+++ assets/utility/AncientMetagenomeDir_LibraryMetadata_Generator.R
@@ -0,0 +1,45 @@
+#! /usr/bin/env Rscript
+
+# https://github.com/docopt/docopt.R
+
+args <- commandArgs(trailingOnly = TRUE)
+
+input_tab <- args[1]
+verbose <- args[2]
+getwd()
+## Add check of required 
+require(tidyverse)
+require(jsonlite)
+require(httr)
+require(xml2)
+require(progress)
+
+source("library_pull_data.R")
+source("library_pull_functions.R")
+
+## Load table -> TEST WHEN DO NOT FILTER TO ENA/SRA
+print("[AncientMetagenomeDir_LibraryMetadata_Generator.R] Only pulling ENA/SRA data!")
+hostass_meta <- read_tsv(input_tab) %>%
+  filter(archive == "ENA" | archive == "SRA")
+
+## Get RUN metadata
+pb <- progress_bar$new(total = nrow(hostass_meta),
+                       format = "(:spin) Getting run metadata [:bar] :percent eta: :eta",
+                       clear = FALSE, width = 60
+)
+hostass_samacc <- hostass_meta %>%
+  mutate(run_metadata = map(archive_accession, ~ {pb$tick(); get_run_from_sample_metadata(.x, v = F)})) %>%
+  unnest(cols = c(run_metadata))
+
+## Get EXPERIMENT metadata using experiment IDs stored in RUN table
+pb <- progress_bar$new(total = nrow(hostass_samacc),
+                       format = "(:spin) Getting experiment metadata [:bar] :percent eta: :eta",
+                       clear = FALSE, width = 60
+)
+hostass_expacc <- hostass_samacc %>% mutate(exp_metadata = map(experiment_accession, ~ {pb$tick(); get_experiment_metadata(.x, v = F)})) %>% unnest()
+
+## Select only relevant columns and save
+hostass_expacc %>% column_cleanup(cols_of_interest) %>% write_tsv(paste0(tools::file_path_sans_ext(input_tab), "_librarymetadata.tsv", collapse = ""))
+
+#hostass_expacc %>% select(!!!cols_of_interest) %>% write_tsv("~/Downloads/thedir_libmetadata_v3.tsv")
+
---
+++ assets/utility/library_pull_functions.R
@@ -0,0 +1,110 @@
+## Performs GET on ENA search URL query
+call_ena_home <- function(url, v = FALSE) {
+  if (v == T) {
+    print(paste0("[DEBUG] call_ena_home() - querying URL:", url, collapse = ": "))
+  }
+  url %>%
+    GET(url = .) %>%
+    content(encoding = "UTF-8") # %>% read_tsv
+}
+
+## Drops certain non-tidy entries in an ENA XML and converts to table
+clean_experiment_xml <- function(x, fields, v = F) {
+  if (v == T) {
+    print("[DEBUG] clean_experiment_xml() - Cleaning XML")
+  }
+  
+  results_tab <- x %>%
+    unlist() %>%
+    enframe() %>%
+    pivot_wider(names_from = name, values_from = value, values_fn = list)
+  
+  avail_fields <- names(results_tab)
+  requested_fields <- fields
+  
+  ## Get ONLY the correct fields from our database
+  final_exp_fields <- avail_fields[avail_fields %in% requested_fields]
+  final_req_fields <- requested_fields[requested_fields %in% final_exp_fields]
+  
+  results_tab %>%
+    select(final_exp_fields) %>%
+    rename(!!!final_req_fields)
+}
+
+column_cleanup <- function(x, fields, v = FALSE) {
+  if (v == T) {
+    print("[DEBUG] ena_column_cleanup() - Selecting preferred fields")
+  }
+  x %>% select(all_of(fields))
+}
+
+
+## Get RUN metadata from a given ENA accession ID (e.g. ERS123456)
+get_run_from_sample_metadata <- function(accession, v = FALSE) {
+  if (v == T) {
+    print(paste("[DEBUG] get_run_from_sample_metadata() - querying accession", accession))
+  }
+
+  ## Split anything combined by commas, as api only accepts one at a time
+  if (v == T) {
+    print("[DEBUG] get_run_from_sample_metadata() - any combined splitting accessions")
+  }
+  all_accs <- accession %>%
+    str_split(",") %>%
+    unlist()
+
+  if (v == T) {
+    print("[DEBUG] get_run_from_sample_metadata() - generating all URLs")
+  }
+  acc_urls <- paste0("https://www.ebi.ac.uk/ena/portal/api/filereport?accession=", all_accs, "&result=read_run&fields=", ena_run_fields)
+
+  if (v == T) {
+    print("[DEBUG] get_run_from_sample_metadata() - querying ENA...")
+  }
+  query_res <- map(acc_urls, ~ call_ena_home(.x, v = v))
+
+  if (v == T) {
+    print("[DEBUG] get_run_from_sample_metadata() - converting to table...")
+  }
+  final_tab <- map(query_res, ~ read_tsv(.x) %>% mutate(across(everything(), as.character))) %>% bind_rows()
+
+  return(final_tab)
+}
+
+## Gets experiment metadata from EXPERIMENT accession codes, as some library
+## metadata is stored at EXPERIMENT level
+get_experiment_metadata <- function(accession, v = FALSE) {
+  if (v == T) {
+    paste0("[DEBUG] get_experiment_metadata() - searching accession:", accession)
+  }
+  if (str_sub(1, 3, string = accession) == "ERX") {
+    user_fields <- ena_exp_fields
+  } else if (str_sub(1, 3, string = accession) == "SRX") {
+    user_fields <- sra_exp_fields
+  } else {
+    return(NA)
+    print(paste0("[ERROR] get_experiment_metadata() - accession starts neither with ERX nor SRX:", accession))
+    #stop(paste0("[ERROR] get_experiment_metadata() - accession starts neither with ERX nor SRX:", accession))
+  }
+
+  if (v == TRUE) {
+    print(paste0("[DEBUG] get_experiment_metadata() - Pulling experiment XML: ", accession))
+  }
+  exp_res <- paste0("https://www.ebi.ac.uk/ena/browser/api/xml/", paste0(accession, collapse = ","), "?download=false&gzip=false") %>%
+    GET(url = .) %>%
+    content(encoding = "UTF-8") %>%
+    as_list()
+
+  if (v == TRUE) {
+    print("[DEBUG] get_experiment_metadata() - Removing nasty experiment attributes")
+  }
+  
+  
+  filtered_results <- map(exp_res$EXPERIMENT_SET, ~ clean_experiment_xml(.x, user_fields, v = v)) %>%
+    reduce(bind_rows) %>%
+    select(-contains("EXPERIMENT_LINK"), -contains("EXPERIMENT_ATTRIBUTE"), ) %>%
+    unnest() %>%
+    unnest()
+
+  return(filtered_results)
+}

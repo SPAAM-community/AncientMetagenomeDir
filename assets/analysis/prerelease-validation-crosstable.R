a6823602c10717fb1607d573fc63c43e2a13c4cd
James Fellows Yates <jfy133@gmail.com>
2023-06-30 21:58:32 +0200

    Fix Rigou/Klapper env table inconsistencies

--- assets/analysis/prerelease-validation-crosstable.R
+++ assets/analysis/prerelease-validation-crosstable.R
@@ -2,21 +2,25 @@
 library(argparser)
 suppressPackageStartupMessages(library(tidyverse))
 
-parser <- argparser::arg_parser('Outputs list of project_names with incosistencies',
-                                 name = 'prerelease-validation-manual.R')
+parser <- argparser::arg_parser("Outputs list of project_names with incosistencies",
+    name = "prerelease-validation-manual.R"
+)
 
-parser <- add_argument(parser, 'samplesTable',
-                       type='character',
-                       nargs=1,
-                       help='Path to the samples.tsv')
-parser <- add_argument(parser, 'libraryTable',
-                       type="character",
-                       nargs=1,
-                       help='Path to the libraries.tsv')
-parser <- add_argument(parser, 'outputTable',
-                       type='character',
-                       nargs=1,
-                       help='Path, including the name, to the output file')
+parser <- add_argument(parser, "samplesTable",
+    type = "character",
+    nargs = 1,
+    help = "Path to the samples.tsv"
+)
+parser <- add_argument(parser, "libraryTable",
+    type = "character",
+    nargs = 1,
+    help = "Path to the libraries.tsv"
+)
+parser <- add_argument(parser, "outputTable",
+    type = "character",
+    nargs = 1,
+    help = "Path, including the name, to the output file"
+)
 
 argv <- parse_args(parser)
 
@@ -27,4 +31,6 @@ write_tsv(allCombined, file = argv$outputTable)
 publication_doi_na <- allCombined %>% filter(is.na(publication_doi))
 cat("\n")
 cat("POSSIBLE PROBLEMS FOR:", paste0(unique(publication_doi_na$project_name), sep = ","), "\n")
+cat("EXCEPTIONS ALLOWED:\n")
+cat("-(ancientmetagenome-hostassociated): Klapper2023 [reseq. of FellowsYates2023 - no new samples]\n")
 cat("\n")

7af045a4711c3d14ccb7e75d2ff9e62e8fb791ce
James A. Fellows Yates <jfy133@gmail.com>
2024-02-12 22:10:28 +0100

    Draft autofill wrapper (bash)

---
+++ assets/utility/autofill-wrapper.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+##
+## AMDirT autofill wrapper for github actions
+##
+## Given a specified table and a project name, this script will
+##
+## 0. Check the specified table is valid name
+## 1. filter the specified sample table for the given project name.
+## 2. collect collect the sample accession IDs of that project
+## 3. give error if there is no output (i.e., 0 accessions)
+## 4. run AMDirT autofill with the collected accessions and the specified table name
+##
+## Copyright 2024 James Fellows Yates on behalf of the SPAAM community under MIT license
+usage() {
+    echo "Usage: $0 [-t <ancientmetagenome-hostassociated|ancientmetagenome-environmental|ancientsingle-hostassociated>] [-p <project_name>]" 1>&2
+    exit 1
+}
+
+valid_tables=("ancientmetagenome-hostassociated" "ancientmetagenome-environmental" "ancientsingle-hostassociated")
+verbose=false
+
+while getopts t:p:v flag; do
+    case "${flag}" in
+    v) verbose=true ;;
+    t) table_name=${OPTARG} ;;
+    p) project_name=${OPTARG} ;;
+    h | *)
+        usage
+        exit 0
+        ;;
+    esac
+done
+
+if ! [[ "${valid_tables[*]}" =~ $table_name ]]; then
+    echo "ERROR: $table_name not valid! Please check input to -t"
+    exit 1
+fi
+
+if [ $verbose ]; then
+    echo RUNNING: grep "$project_name" "$table_name"/samples/"$table_name"_samples.tsv "| rev | cut -f 1 | rev | tr '\n' ' '"
+    echo ""
+fi
+
+hits=$(grep "$project_name" "$table_name"/samples/"$table_name"_samples.tsv | rev | cut -f 1 | rev | tr '\n' ' ')
+
+if [ $verbose ]; then
+    echo RUNNING: "${#hits} -eq 0"
+    echo ""
+fi
+
+if [ ${#hits} -eq 0 ]; then
+    echo 'ERROR: Project name or sample accessions were not found in table. Please check project name input (-p).'
+    exit 1
+fi
+
+if [ $verbose ]; then
+    echo RUNNING: AMDirT autofill -n "$table_name" "$hits" -l TEMPLATE_"$project_name"_"$table_name"_libraries.tsv
+    echo ""
+fi
+
+AMDirT autofill -n "$table_name" "$hits" -l TEMPLATE_"$project_name"_"$table_name"_libraries.tsv

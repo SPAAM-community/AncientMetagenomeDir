2c378e1c0d81b60f1d711f07d2e4c4a2dbcf6729
James A. Fellows Yates <jfy133@gmail.com>
2024-04-08 14:18:44 +0200

    Improve prelease validation withintabgle script

--- assets/analysis/prerelease-validation-withintable.R
+++ assets/analysis/prerelease-validation-withintable.R
@@ -1,5 +1,11 @@
 library(tidyverse)
 
+##################
+## DATA LOADING ##
+##################
+
+## PREP: Load raw data ##
+
 senv <-
   read_tsv(
     "ancientmetagenome-environmental/samples/ancientmetagenome-environmental_samples.tsv"
@@ -26,9 +32,11 @@ lsin <-
     "ancientsinglegenome-hostassociated/libraries/ancientsinglegenome-hostassociated_libraries.tsv"
   )
 
-###############################
-## archive_accession mismatch between tables? ###
-###############################
+#################
+## START TESTS ##
+#################
+
+## PREP: load data for archive_accession mismatch between tables? ###
 
 senv_acc <- senv$archive_accession %>%
   strsplit(",") %>%
@@ -53,12 +61,12 @@ lsin_acc <-
   strsplit(",") %>%
   unlist()
 
-## Archive accession in sample, not library (missing)
+## TEST: Archive accession in sample, not library (missing): if no errors should result in character(0)
 setdiff(senv_acc, lenv_acc)
 setdiff(shos_acc, lhos_acc)
 setdiff(ssin_acc, lsin_acc)
 
-## Archive accession in library, not sample (unexpected extra)
+## TEST: Archive accession in library, not sample (unexpected extra): if no errors should result in character(0)
 setdiff(lenv_acc, senv_acc)
 setdiff(lhos_acc, shos_acc)
 setdiff(lsin_acc, ssin_acc)
@@ -66,6 +74,8 @@ setdiff(lsin_acc, ssin_acc)
 ###############################
 ## DOI mismatch between tables? ###
 ###############################
+
+## Prepare information for comparison ##
 
 senv_doi <- senv$publication_doi %>%
   strsplit(",") %>%
@@ -87,12 +97,13 @@ lsin_doi <- lsin$data_publication_doi %>%
   strsplit(",") %>%
   unlist()
 
-## Archive accession in sample, not library (missing)
+
+## TEST: Archive accession in sample, not library (missing): if no errors should result in character(0)
 setdiff(senv_doi, lenv_doi)
 setdiff(shos_doi, lhos_doi)
 setdiff(ssin_doi, lsin_doi)
 
-## Archive accession in library, not sample (unexpected extra)
+## TEST: Archive accession in library, not sample (unexpected extra): if no errors should result in character(0)
 ## Exception allowed: 10.1126/science.adf5300 (Klapper2023 reseq of FellowsYates2021 so no new samples)
 setdiff(lenv_doi, senv_doi)
 setdiff(lhos_doi, shos_doi)
@@ -102,7 +113,9 @@ setdiff(lsin_doi, ssin_doi)
 ## archive_accession duplicates? ###
 ####################################
 
-## SAMPLES - expect one per accession
+## TEST: samples - expect one per accession: if no errors should result in A tibble 0 x 2
+
+## For env: metagenome execeptions allow: none so far!
 senv_dups <-
   senv$archive_accession %>%
   str_split(",") %>%
@@ -114,7 +127,7 @@ senv_dups <-
   filter(n > 1) %>%
   print(n = 1000)
 
-## Exception allowed: McDonough2018 [sample accession == museum specimen],
+## For host ass. metagenome exceptions allowed: McDonough2018 [sample accession == museum specimen]
 shos_dups <-
   shos$archive_accession %>%
   str_split(",") %>%
@@ -124,16 +137,15 @@ shos_dups <-
   summarise(n = n()) %>%
   arrange(desc(n)) %>%
   filter(n > 1) %>%
-  filter(!archive_accession %in% c("SRS3118688", "SRS3118689", "SRS3151295")) %>% ## excetions!
+  filter(!archive_accession %in% c("SRS3118688", "SRS3118689", "SRS3151295")) %>%
   print(n = 1000)
 
-## Exception allowed: Schuenemann2018/KrauseKyora2018b [resequencing], Krause-Kyora/Lugli [reanalysis],
+## For single genome: exception allowed: Schuenemann2018/KrauseKyora2018b [resequencing], Krause-Kyora/Lugli [reanalysis],
 ## DeDios2020 [multi-species], Devault2017 [multi-species], Austin2022 [multi-species],
 ## Guellil2022 [multi-species], Bonczarowska2022 [multi-species]
 ## SeguinOrlando2021, Clavel2023 [sample re-use/reqseuqencing]
 ## Hodgkins2023 [sample re-use]
 ssin_dup_exceptions <- c("ERS942272", "ERS942281", "ERS942282", "ERS4278128", "ERS4278129", "ERS4278130", "ERS942276", "SRS1779840", "SRS1779841", "SRS1779844", "SRS1779846", "SRS13524932", "ERS8968539", "ERS9069041", "ERS9069042", "ERS5823072", "ERS5823096", "ERS1020860", "ERS1124787", "ERS1892067", "ERS2020227", "ERS2865271", "ERS4266549", "ERS563409", "SRS591994", "SRS592075")
-
 
 ssin_dups <-
   ssin$archive_accession %>%
